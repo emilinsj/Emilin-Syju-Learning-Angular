bc3bd06b0b70540e86edb8c140251ae99f9bc9df
Emilin Syju <w0847462@myscc.ca>
2024-09-23 22:10:00 -0400

    Created 2 new components

---
+++ src/app/phone-list-item/phone-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-list-item.component.html',
+  styleUrl: './phone-list-item.component.css'
+})
+export class PhoneListItemComponent {
+
+}
