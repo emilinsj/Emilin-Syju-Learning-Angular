c0bf79603a6228a17d18f07e29e974773ee6fb30
Emilin Syju <w0847462@myscc.ca>
2024-09-25 16:42:18 -0400

    added data to each tags

--- src/app/phone-list/phone-list.component.ts
+++ src/app/phone-list/phone-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Phone} from "../models/phone";
 import {PhoneListItemComponent} from "../phone-list-item/phone-list-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-phone-list',
   standalone: true,
   imports: [
-    PhoneListItemComponent
+    PhoneListItemComponent,
+    NgForOf
   ],
   templateUrl: './phone-list.component.html',
   styleUrl: './phone-list.component.css'
@@ -16,10 +18,10 @@ export class PhoneListComponent {
   PhoneList: Phone[]=[
     { serialNumber:1,brand:"Apple",name:"Iphone 15",color:"light Blue",software:"ios"},
     { serialNumber:2,brand:"Samsung",name:"Samsung s23",color:"Black",software:"Android"},
-    { serialNumber:1,brand:"Huawei",name:"Pura 70",color:"Pink",software:"Android"},
-    { serialNumber:1,brand:"Lenovo",name:"K14 Plus",color:"Grey",software:"Android"},
-    { serialNumber:1,brand:"Microsoft",name:"Lumia 640 XL",color:"White",software:"Android"},
-    { serialNumber:1,brand:"Apple",name:"Iphone SE",color:"Red",software:"ios"}
+    { serialNumber:3,brand:"Huawei",name:"Pura 70",color:"Pink",software:"Android"},
+    { serialNumber:4,brand:"Lenovo",name:"K14 Plus",color:"Grey",software:"Android"},
+    { serialNumber:5,brand:"Microsoft",name:"Lumia 640 XL",color:"White",software:"Android"},
+    { serialNumber:6,brand:"Apple",name:"Iphone SE",color:"Red",software:"ios"}
   ];
 
 }
