aaae20845f9732306d06755e86899191771ddc28
Emilin Syju <w0847462@myscc.ca>
2024-09-25 20:19:40 -0400

    updated html and css file to add bg color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Phone} from "./models/phone";
+/*import {Phone} from "./models/phone";*/
 import {NgIf} from "@angular/common";
 import {PhoneListComponent} from "./phone-list/phone-list.component";
 
@@ -13,15 +13,5 @@ import {PhoneListComponent} from "./phone-list/phone-list.component";
 })
 export class AppComponent {
   title = 'Emilin Phone collection';
-  PhoneList: Phone[]=[
-    { serialNumber:1,brand:"Apple",name:"Iphone 15",color:"light Blue",software:"ios"},
-    { serialNumber:2,brand:"Samsung",name:"Samsung s23",color:"Black",software:"Android"},
-    { serialNumber:1,brand:"Huawei",name:"Pura 70",color:"Pink",software:"Android"},
-    { serialNumber:1,brand:"Lenovo",name:"K14 Plus",color:"Grey",software:"Android"},
-    { serialNumber:1,brand:"Microsoft",name:"Lumia 640 XL",color:"White",software:"Android"},
-    { serialNumber:1,brand:"Apple",name:"Iphone SE",color:"Red",software:"ios"},
-
-  ]
-
 
 }
