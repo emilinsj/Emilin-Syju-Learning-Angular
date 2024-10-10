7966a78efce6a082c891c3e9b28df30fff57ebcd
Emilin Syju <w0847462@myscc.ca>
2024-10-10 19:30:25 -0400

    created mock data and service

---
+++ src/app/models/mock-phone.ts
@@ -0,0 +1,12 @@
+import {Phone} from "./phone";
+
+
+export const PhoneList: Phone[]=[
+    { serialNumber:1,brand:"Apple",name:"Iphone 15",color:"light Blue",software:"ios"},
+{ serialNumber:2,brand:"Samsung",name:"Samsung s23",color:"Black",software:"Android"},
+{ serialNumber:3,brand:"Huawei",name:"Pura 70",color:"Pink",software:"Android"},
+{ serialNumber:4,brand:"Lenovo",name:"K14 Plus",color:"Grey",software:"Android"},
+{ serialNumber:5,brand:"Microsoft",name:"Lumia 640 XL",color:"White",software:"Android"},
+{ serialNumber:6,brand:"Apple",name:"Iphone SE",color:"Red",software:"ios"}
+];
+
--- src/app/phone-list/phone-list.component.ts
+++ src/app/phone-list/phone-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Phone} from "../models/phone";
+//import {Phone} from "../models/phone";
 import {PhoneListItemComponent} from "../phone-list-item/phone-list-item.component";
 import {NgForOf} from "@angular/common";
 
@@ -15,13 +15,13 @@ import {NgForOf} from "@angular/common";
 })
 export class PhoneListComponent {
   display:string[]=["serialNumber","brand","name","color","software"];
-  PhoneList: Phone[]=[
+  /*PhoneList: Phone[]=[
     { serialNumber:1,brand:"Apple",name:"Iphone 15",color:"light Blue",software:"ios"},
     { serialNumber:2,brand:"Samsung",name:"Samsung s23",color:"Black",software:"Android"},
     { serialNumber:3,brand:"Huawei",name:"Pura 70",color:"Pink",software:"Android"},
     { serialNumber:4,brand:"Lenovo",name:"K14 Plus",color:"Grey",software:"Android"},
     { serialNumber:5,brand:"Microsoft",name:"Lumia 640 XL",color:"White",software:"Android"},
     { serialNumber:6,brand:"Apple",name:"Iphone SE",color:"Red",software:"ios"}
-  ];
+  ];*/
 
 }
