09c7d934f5cea259de2877266ac00a7ba3519488
Emilin Syju <w0847462@myscc.ca>
2024-10-16 23:04:15 -0400

    checked the router and added a list of routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {PhoneListComponent} from "./app/phone-list/phone-list.component";
+import {PhoneListItemComponent} from "./app/phone-list-item/phone-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes:Routes=[
+  {path:'phones', component:PhoneListComponent},
+  {path:'phones/:serialNumber', component:PhoneListItemComponent}
+
+]
+
+bootstrapApplication(AppComponent,{
+  providers:[provideRouter(routes)]
+}).catch((err) => console.error(err));
