a441fc3d1dfa231066b56d7e3678ebdefbe1adcf
Emilin Syju <w0847462@myscc.ca>
2024-11-03 15:08:35 -0500

    created InMemoryDataService and defines the data

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Phone} from "../models/phone";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService{
+  createDb(): {phones: Phone[]} {
+    const phones:Phone[]=[
+      { serialNumber:1,brand:"Apple",name:"Iphone 15",color:"light Blue",software:"ios",path:'/../images/iphone15.jpg'},
+      { serialNumber:2,brand:"Samsung",name:"Samsung s23",color:"Black",software:"Android",path:'/../images/s23.jpg'},
+      { serialNumber:3,brand:"Huawei",name:"Pura 70",color:"Pink",software:"Android",path:'/../images/pura70.jpg'},
+      { serialNumber:4,brand:"Lenovo",name:"K14 Plus",color:"Grey",software:"Android",path:'/../images/lenova.jpg'},
+      { serialNumber:5,brand:"Microsoft",name:"Lumia 640 XL",color:"White",software:"Android",path:'/../images/micro.jpg'},
+      { serialNumber:6,brand:"Apple",name:"Iphone SE",color:"Red",software:"ios", path:'/../images/se.jpg'}
+    ];
+    return {phones};
+  }
+
+}
