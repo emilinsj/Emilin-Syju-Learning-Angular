e474b5d8c334e8f753285c8b6161b8b4edf5971e
Emilin Syju <w0847462@myscc.ca>
2024-11-03 15:18:51 -0500

    updated all crud operations using HTTP requests

--- src/app/services/phone.service.ts
+++ src/app/services/phone.service.ts
@@ -1,36 +1,33 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from "rxjs";
+import { Observable} from "rxjs";
 import {Phone} from "../models/phone";
 import {PhoneList} from "../models/mock-phone";
+import {HttpClient} from "@angular/common/http";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhoneService {
+  private apiUrl='api/phones';
   private phones:Phone[]=PhoneList;
-  constructor() { }
+  constructor(private http: HttpClient) { }
   getPhones(): Observable<Phone[]>{
-    return of(PhoneList);
+    return this.http.get<Phone[]>(this.apiUrl);
   }
-  getPhoneById(phoneId: number): Observable<Phone | undefined>{
-    const phone=this.phones.find(user=>user.serialNumber===phoneId);
-    return of(phone);
+  getPhoneById(phoneId: number): Observable<Phone>{
+    return this.http.get<Phone>(`${this.apiUrl}/${phoneId}`);
   }
-  addPhone(newPhone:Phone): Observable<Phone[]>{
-    this.phones.push(newPhone)
-    return of(this.phones);
+  addPhone(newPhone:Phone): Observable<Phone>{
+    return this.http.post<Phone>(this.apiUrl, newPhone);
   }
-  updatePhone(updatedPhone: Phone): Observable<Phone[]> {
-    const index = this.phones.findIndex(user => user.serialNumber === updatedPhone.serialNumber);
-    if (index !== -1) {
-      this.phones[index] = updatedPhone;
-    }
-    return of(this.phones);
+  updatePhone(updatedPhone: Phone): Observable<Phone | undefined> {
+    const url = `${this.apiUrl}/${updatedPhone.serialNumber}`;
+    return this.http.put<Phone>(url,updatedPhone);
   }
-  deletePhone(PhoneId: number): Observable<Phone[]> {
-    this.phones = this.phones.filter(user => user.serialNumber !== PhoneId);
-    return of(this.phones);
+  deletePhone(PhoneId: number): Observable<{}> {
+    const url = `${this.apiUrl}/${PhoneId}`;
+    return this.http.delete(url)
   }
   generateNewID():number{
     return this.phones.length > 0 ? Math.max(...this.phones.map(phone => phone.serialNumber)) + 1 : 1;
